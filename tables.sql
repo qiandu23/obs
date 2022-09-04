afd221ed51fa2286358293113e6d78ee69938084
qiandu <1021424011@qq.com>
2022-09-04 22:41:29 +0800

    Add tables.sql in sqlite.

---
+++ tables.sql
@@ -0,0 +1,30 @@
+create table account
+(
+    id          TEXT    not null
+        primary key,
+    name        TEXT    not null
+        unique,
+    password    TEXT    not null,
+    is_admin    INTEGER not null,
+    description TEXT,
+    update_id   TEXT    not null,
+    update_date TEXT    not null
+);
+
+create table storage
+(
+    id          TEXT not null,
+    name        TEXT not null
+        constraint storage_pk
+            unique,
+    endpoint    TEXT not null,
+    access_key  TEXT not null,
+    secret_key  TEXT not null,
+    update_id   TEXT not null,
+    update_date TEXT not null,
+    region      TEXT
+);
+
+create unique index sqlite_autoindex_storage_1
+    on storage (id);
+
