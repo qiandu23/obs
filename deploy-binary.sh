8096bfba9c2f6e439c14da7191f006ed05ef27f6
qiandu <1021424011@qq.com>
2023-08-06 19:08:49 +0800

    Add linux binary deploy script.

--- deploy-binary.sh
+++ deploy-binary.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env sh
 
+username=obs-ui
 create_user(){
-  username=obs-ui
   id "$username" &>/dev/null
   result=$?
 
   if [ "$result" -eq 0 ]; then
     echo "Account $username exists."
   else
-    sudo adduser --system --no-create-home $username
-    sudo usermod -aG sudo $username
+    sudo adduser --system --no-create-home $username && \
+    sudo usermod -aG sudo $username && \
+    echo "Account $username created."
   fi
 }
 
-create_user
+build_pkg(){
+  if [ ! -d "node_modules" ]; then
+    if [ -f "package-lock.json" ]; then
+      rm package-lock.json
+    fi
+
+    npm i
+  fi
+
+  pkg -t node14-linux-x64 -o obs-ui . && \
+  sudo mkdir -p /usr/local/lib/obs-ui && mv obs-ui /usr/local/lib/obs-ui/ && mv tables.sql /usr/local/lib/obs-ui/ &&\
+  sudo sqlite3 /usr/local/lib/obs-ui/object-storage-browser.sqlite < /usr/local/lib/obs-ui/tables.sql && \
+  sudo chown -R $username:$username /usr/local/lib/obs-ui
+}
+
+create_user && build_pkg
 
