7149a36241c95748021c2c8a7043786a4c6e58df
qiandu <1021424011@qq.com>
2023-08-06 18:53:49 +0800

    Add linux binary deploy script.

--- deploy-binary.sh
+++ deploy-binary.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env sh
 
-username=obs-ui
-id "$username" &>/dev/null
-result=$?
+create_user(){
+  username=obs-ui
+  id "$username" &>/dev/null
+  result=$?
 
-if [ "$result" -eq 0 ]; then
-  echo "Account $username exists."
-else
-  sudo adduser --system --no-create-home $username
-  sudo usermod -aG sudo $username
-fi
+  if [ "$result" -eq 0 ]; then
+    echo "Account $username exists."
+  else
+    sudo adduser --system --no-create-home $username
+    sudo usermod -aG sudo $username
+  fi
+}
+
+create_user
+
