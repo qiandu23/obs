7c24b313fb8a34d4034e3275e1070318f527e817
qiandu <1021424011@qq.com>
2023-08-06 19:16:34 +0800

    Add linux binary deploy script.

--- deploy-binary.sh
+++ deploy-binary.sh
@@ -14,7 +14,7 @@ create_user(){
   fi
 }
 
-build_pkg(){
+npm_build(){
   if [ ! -d "node_modules" ]; then
     if [ -f "package-lock.json" ]; then
       rm package-lock.json
@@ -22,9 +22,16 @@ build_pkg(){
 
     npm i
   fi
+}
 
-  pkg -t node14-linux-x64 -o obs-ui . && \
-  sudo mkdir -p /usr/local/lib/obs-ui && mv obs-ui /usr/local/lib/obs-ui/ && mv tables.sql /usr/local/lib/obs-ui/ &&\
+build_pkg(){
+  (cd server && npm_build && pkg -t node14-linux-x64 -o obs-ui .)
+  (cd ui && rm -rf dist && npm_build && npm run build)
+  (rm -rf obs-ui dist && cp -rf server/obs-ui . && cp -rf ui/dist .)
+  pkg -t node14-linux-x64 -o obs-ui .
+
+  sudo mkdir -p /usr/local/lib/obs-ui && mv obs-ui /usr/local/lib/obs-ui/ && \
+  mv dist /usr/local/lib/obs-ui/ && mv tables.sql /usr/local/lib/obs-ui/ &&\
   sudo sqlite3 /usr/local/lib/obs-ui/object-storage-browser.sqlite < /usr/local/lib/obs-ui/tables.sql && \
   sudo chown -R $username:$username /usr/local/lib/obs-ui
 }
