b9b108baf994afe46e257f26ad72de0c91849b17
qiandu <1021424011@qq.com>
2023-08-07 10:07:51 +0800

    Add linux binary deploy script.

--- deploy-binary.sh
+++ deploy-binary.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 username=obs-ui
+deploy_directory=/usr/local/lib/obs-ui
+
 create_user(){
   id "$username" &>/dev/null
   result=$?
@@ -28,12 +30,15 @@ build_pkg(){
   (cd server && npm_build && pkg -t node14-linux-x64 -o obs-ui .)
   (cd ui && rm -rf dist && npm_build && npm run build)
   (rm -rf obs-ui dist && cp -rf server/obs-ui . && cp -rf ui/dist .)
-  pkg -t node14-linux-x64 -o obs-ui .
 
-  sudo mkdir -p /usr/local/lib/obs-ui && mv obs-ui /usr/local/lib/obs-ui/ && \
-  mv dist /usr/local/lib/obs-ui/ && mv tables.sql /usr/local/lib/obs-ui/ &&\
-  sudo sqlite3 /usr/local/lib/obs-ui/object-storage-browser.sqlite < /usr/local/lib/obs-ui/tables.sql && \
-  sudo chown -R $username:$username /usr/local/lib/obs-ui
+  if [ ! -d "$deploy_directory" ]; then
+    sudo mkdir -p $deploy_directory
+  fi
+
+  rm -rf $deploy_directory/dist && \
+  cp obs-ui $deploy_directory && mv dist $deploy_directory && cp docker/tables.sql $deploy_directory && \
+  sudo sqlite3 $deploy_directory/object-storage-browser.sqlite < $deploy_directory/tables.sql && \
+  sudo chown -R $username:$username $deploy_directory
 }
 
 create_user && build_pkg
